41bf98236241641f4afbcd0f2f6fc6a413d3b87b
KRYPTONITE\k.zamotin <k.zamotin@kryptonite.ru>
2024-08-05 15:27:48 +0300

    feat: morse decode

--- src/index.js
+++ src/index.js
@@ -36,9 +36,41 @@ const MORSE_TABLE = {
     '----.':  '9',
     '-----':  '0',
 };
-
 function decode(expr) {
     // write your solution here
+    
+    var result=""
+    
+    for (let i = 0; i < expr.length; i+=10) {
+       var letter=expr.slice(i,i+10);
+       
+       console.log(letter)
+       
+       var morse_code = ""
+       
+       if (letter === "**********") {
+           result += " "
+       }
+       else{
+           for (let j = 0; j < letter.length; j+=2) {
+               var code=letter.slice(j,j+2);
+               console.log(code)
+               
+               if (code ==="10"){
+                   morse_code +="."
+               }
+               if (code ==="11"){
+                   morse_code +="-"
+               }
+               
+           }
+           result += MORSE_TABLE[morse_code]
+           console.log(morse_code)
+        }   
+        
+    }
+    
+    return result
 }
 
 module.exports = {
